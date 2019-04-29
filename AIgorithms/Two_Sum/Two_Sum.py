3f0449a9227c66643bb71af7d9ce160342b7d718
Vickibreeze <caolijia0303@126.comwq>
2019-04-29 23:34:10 +0800

    Move Tow_Sum.py to directory Tow_Sum
    Signed-off-by: Vickibreeze <caolijia0303@126.comwq>

--- AIgorithms/Two_Sum/Two_Sum.py
+++ AIgorithms/Two_Sum/Two_Sum.py
@@ -5,11 +5,11 @@ You may assume that each input would have exactly one solution, and you may not 
 
 Example:
 
-Given nums = [2, 7, 11, 15], target = 9,
-
-Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+    Given nums = [2, 7, 11, 15], target = 9,
+    Because nums[0] + nums[1] = 2 + 7 = 9,
+    return [0, 1].
 '''
+
 
 class Solution(object):
     def twoSum(self, nums, target):
